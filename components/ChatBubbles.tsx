890fff3b92926f8cac9c8a7e3aa58867e1cadea0
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-04-05 22:57:49 +0900

    Finalized the default positioning of the user's chat bubble

--- components/ChatBubbles.tsx
+++ components/ChatBubbles.tsx
@@ -6,7 +6,11 @@ type Props = {
 const ChatBubbles: React.FC<Props> = ({ userInput }) => {
   return (
     <>
-      <div className="text-white">{userInput}</div>
+      <div className="relative w-full max-w-2xl mt-10">
+        <div className="bg-[rgb(45,45,45)] text-white rounded-[1rem] absolute right-0 p-3">
+          {userInput}
+        </div>
+      </div>
     </>
   );
 };
--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [userInput, setUserInput] = useState<string>("");
   return (
     <>
-      <div className="relative w-screen h-screen flex bg-cover bg-[rgb(30,30,30)] justify-center">
+      <div className="w-screen h-screen bg-[rgb(30,30,30)] flex flex-col items-center justify-end ">
         <ChatBubbles userInput={userInput} />
         <MainTextbox setUserInput={setUserInput} />
       </div>
