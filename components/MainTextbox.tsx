8a07cb3fb46ae7e3210f67a3a2e61b33dce08cdc
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-04-05 21:32:16 +0900

    First commit

---
+++ components/MainTextbox.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { useState, useRef, useEffect, ChangeEvent } from "react";
+import iconAddWhite from "../assets/icons/add-white.svg";
+import iconUpWhite from "../assets/icons/up-white.svg";
+
+const MainTextbox: React.FC = () => {
+  const [text, setText] = useState<string>("");
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const handleResize = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+  const handleButtonUp = () => {
+    setText("");
+  };
+
+  useEffect(() => {
+    //#region adjust textbox height
+    const textarea = textareaRef.current;
+    const container = containerRef.current;
+    if (textarea && container) {
+      textarea.style.height = "auto";
+      if (textarea.scrollHeight < 200) {
+        textarea.style.height = `${textarea.scrollHeight}px`;
+        container.style.height = `${textarea.scrollHeight + 40}px`;
+      } else {
+        textarea.style.height = `160px`;
+        container.style.height = `200px`;
+      }
+    }
+    //#endregion
+    //#region handle submit text logic
+    const handleKeyPress = (e: KeyboardEvent) => {
+      if (e.ctrlKey && e.key === "Enter") {
+        setText("");
+      }
+    };
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+    //#endregion
+  }, [text]);
+
+  return (
+    <>
+      <div className="relative w-full max-w-2xl mt-auto mb-10">
+        <div
+          ref={containerRef}
+          className="relative w-full rounded-[1rem] bg-[rgb(45,45,45)]"
+        >
+          <textarea
+            ref={textareaRef}
+            value={text}
+            onChange={handleResize}
+            className="
+              w-full pt-2 px-5 resize-none overflow-y-auto 
+              placeholder-[rgb(90,90,90)] text-white
+              focus:outline-none focus:border-none
+            "
+            placeholder="Message"
+          ></textarea>
+          <button
+            className="
+                rounded-full border border-[rgb(60,60,60)] cursor-pointer
+                p-1 ml-4 bottom-2 left-0 absolute
+                transition duration-300 hover:scale-120 hover:border-white
+              "
+          >
+            <img src={iconAddWhite} alt="" />
+          </button>
+          <button
+            className="
+                rounded-full border border-[rgb(60,60,60)] cursor-pointer 
+                p-1 mr-4 bottom-2 right-0 absolute 
+                transition duration-300 hover:scale-120 hover:border-white
+              "
+            onClick={handleButtonUp}
+          >
+            <img src={iconUpWhite} alt="" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainTextbox;
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "../styles/main.css";
+import MainTextbox from "../components/MainTextbox";
+
+function App() {
+  return (
+    <>
+      <div className="relative w-screen h-screen flex bg-cover bg-[rgb(30,30,30)] justify-center">
+        <MainTextbox />
+      </div>
+    </>
+  );
+}
+
+export default App;
