fcd6764b38d41c45c09158ac587ccc1cd3c7ad96
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-04-07 15:19:55 +0900

    Added a copy button for codeblocks highlighted by highlight.js

--- components/CodeblockConverter.tsx
+++ components/CodeblockConverter.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import hljs from "highlight.js";
 import "highlight.js/styles/felipec.css";
 import ReactMarkdown from "react-markdown";
@@ -8,19 +8,33 @@ type Props = {
 };
 
 const CodeblockConverter: React.FC<Props> = ({ inputMessage }) => {
+  const [copyMessage, setCopyMessage] = useState<string>("Copy");
+
+  const handleCopy = (code: string) => {
+    navigator.clipboard.writeText(code);
+    setCopyMessage("Copied");
+    setTimeout(() => {
+      setCopyMessage("Copy");
+    }, 2000);
+  };
+
   const components = {
     code: ({ node, inline, className, children, ...props }: any) => {
       const match = /language-(\w+)/.exec(className || "");
       const language = match && match[1] ? match[1] : "";
 
       if (!inline && language) {
-        try {
-          const highlightedCode = hljs.highlight(
-            String(children).replace(/\n$/, ""),
-            { language }
-          ).value;
+        const code = String(children).replace(/\n$/, "");
+        const highlightedCode = hljs.highlight(code, { language }).value;
 
-          return (
+        return (
+          <div className="relative">
+            <button
+              onClick={() => handleCopy(code)}
+              className="absolute top-2 right-2 z-10 px-2 py-1 text-xs bg-gray-700 text-white rounded hover:bg-gray-600"
+            >
+              {copyMessage}
+            </button>
             <pre>
               <code
                 className={`hljs ${language}`}
@@ -28,10 +42,8 @@ const CodeblockConverter: React.FC<Props> = ({ inputMessage }) => {
                 {...props}
               />
             </pre>
-          );
-        } catch (err) {
-          console.warn("Error highlighting code block:", err);
-        }
+          </div>
+        );
       }
 
       return (
