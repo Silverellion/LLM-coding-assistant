3c52e70969be0de8f91e21307840857c4eefd732
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-04-07 17:52:32 +0900

    Made the sidebar collapsable

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,15 +8,26 @@ type SidebarProps = {
 const Sidebar: React.FC<SidebarProps> = ({ isCollapsed, toggleSidebar }) => {
   return (
     <div
-      className={`fixed top-0 left-0 h-full ${
+      className={`fixed flex flex-row top-0 left-0 h-full ${
         isCollapsed ? "w-[50px]" : "w-[300px]"
       } bg-[rgb(15,15,15)] transition-all duration-300`}
     >
       <button
         onClick={toggleSidebar}
-        className="absolute top-2 right-2 bg-[rgb(45,45,45)] text-white p-2 rounded"
+        className="
+            absolute top-1/2 right-[-15px] transform -translate-y-1/2 
+            bg-[rgb(45,45,45)] text-white p-2 rounded shadow-md cursor-pointer 
+            hover:scale-120 hover:bg-[rgb(37,37,37)] transition-transform duration-300
+        "
       >
-        {isCollapsed ? ">" : "<"}
+        <span
+          className={`
+            inline-block transform transition-transform ease-in-out duration-600
+            ${isCollapsed ? "rotate-0" : "rotate-540"}
+        `}
+        >
+          &#10094;
+        </span>
       </button>
     </div>
   );
